78f52416cdd7e823430d9b55d3274d70893d04d5
David <144546989+dangol-david@users.noreply.github.com>
2024-07-03 21:34:47 +0545

    Add files via upload

---
+++ img/logo3_files/style.php
@@ -0,0 +1,22 @@
+
+.mtli_attachment {
+	display:inline-block;
+	height: auto;
+	min-height:64px;
+	background-position: top left;
+	background-attachment: scroll;
+	background-repeat: no-repeat;
+	padding-left: 76px !important;
+}
+.mtli_ai {
+	background-image: url(../images/ai-icon-64x64.png); }
+.mtli_eps {
+	background-image: url(../images/eps-icon-64x64.png); }
+.mtli_psd {
+	background-image: url(../images/psd-icon-64x64.png); }
+.mtli_svg {
+	background-image: url(../images/svg-icon-64x64.png); }
+.mtli_cdr {
+	background-image: url(../images/cdr-icon-64x64.png); }
+.mtli_pdf {
+	background-image: url(../images/pdf-icon-64x64.png); }
